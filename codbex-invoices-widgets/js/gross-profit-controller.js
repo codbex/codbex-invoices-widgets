82a0614183daae8e1411f72c5a2ea80845dad118
MrGoblings <mrgoblings@gmail.com>
2024-08-16 17:13:06 +0000

    add: js controller files

---
+++ codbex-invoices-widgets/js/gross-profit-controller.js
@@ -0,0 +1,29 @@
+const grossProfitWidget = angular.module('gross-profit', ['ideUI', 'ideView']);
+
+grossProfitWidget.controller('Gross-Profit-Controller', ['$scope', '$http', function ($scope, $http) {
+    $scope.state = {
+        isBusy: true,
+        error: false,
+        busyText: "Loading...",
+    };
+
+    const invoiceServiceUrl = "/services/ts/codbex-hestia/api/InvoiceService.ts/invoiceData";
+    $http.get(invoiceServiceUrl)
+        .then(function (response) {
+            $scope.InvoiceData = response.data;
+            calculateGrossProfit();
+        });
+
+    const orderServiceUrl = "/services/ts/codbex-hestia/api/OrderService.ts/orderData";
+    $http.get(orderServiceUrl)
+        .then(function (response) {
+            $scope.OrderData = response.data;
+            calculateGrossProfit();
+        });
+
+    function calculateGrossProfit() {
+        if ($scope.InvoiceData && $scope.OrderData) {
+            $scope.GrossProfit = (($scope.InvoiceData.SalesInvoiceTotal + $scope.OrderData.SalesOrderTotal) - ($scope.InvoiceData.PurchaseInvoiceTotal + $scope.OrderData.PurchaseOrderTotal)).toFixed(2);
+        }
+    }
+}]);
---
+++ codbex-invoices-widgets/js/total-sales-controller.js
@@ -0,0 +1,101 @@
+const dashboard = angular.module('invoiceWidgets', ['ideUI', 'ideView']);
+
+dashboard.controller('Invoice-Widgets-Controller', ['$scope', '$document', '$http', 'messageHub', function ($scope, $document, $http, messageHub) {
+    $scope.state = {
+        isBusy: true,
+        error: false,
+        busyText: "Loading...",
+    };
+
+    $scope.openPerspective = function (perspective) {
+        if (perspective === 'sales-orders') {
+            messageHub.postMessage('launchpad.switch.perspective', { perspectiveId: 'sales-orders' }, true);
+        } else if (perspective === 'products') {
+            messageHub.postMessage('launchpad.switch.perspective', { perspectiveId: 'products' }, true);
+        } else if (perspective === 'sales-invoices') {
+            messageHub.postMessage('launchpad.switch.perspective', { perspectiveId: 'sales-invoices' }, true);
+        }
+        ;
+    }
+
+    $scope.today = new Date();
+
+    const invoiceServiceUrl = "/services/ts/codbex-hestia/api/InvoiceService.ts/invoiceData";
+    $http.get(invoiceServiceUrl)
+        .then(function (response) {
+            $scope.InvoiceData = response.data;
+            calculateGrossProfit();
+        });
+
+    const orderServiceUrl = "/services/ts/codbex-hestia/api/OrderService.ts/orderData";
+    $http.get(orderServiceUrl)
+        .then(function (response) {
+            $scope.OrderData = response.data;
+            calculateGrossProfit();
+        });
+
+    const productServiceUrl = "/services/ts/codbex-hestia/api/ProductService.ts/productData";
+    $http.get(productServiceUrl)
+        .then(function (response) {
+            $scope.ProductData = response.data;
+        });
+
+    async function getProductData() {
+        try {
+            const response = await $http.get("/services/ts/codbex-hestia/api/ProductService.ts/productData");
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching product data:', error);
+        }
+    }
+
+    function calculateGrossProfit() {
+        if ($scope.InvoiceData && $scope.OrderData) {
+            $scope.GrossProfit = (($scope.InvoiceData.SalesInvoiceTotal + $scope.OrderData.SalesOrderTotal) - ($scope.InvoiceData.PurchaseInvoiceTotal + $scope.OrderData.PurchaseOrderTotal)).toFixed(2);
+        }
+    }
+
+    async function getOrderData() {
+        try {
+            const response = await $http.get("/services/ts/codbex-hestia/api/OrderService.ts/orderData");
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching order data:', error);
+        }
+    }
+
+    angular.element($document[0]).ready(async function () {
+        const orderData = await getOrderData();
+        $scope.topSalesOrders = orderData.TopSalesOrders;
+    });
+
+    angular.element($document[0]).ready(async function () {
+        const orderData = await getOrderData();
+        $scope.topPurchaseOrders = orderData.TopPurchaseOrders;
+    });
+
+    angular.element($document[0]).ready(async function () {
+        const orderData = await getOrderData();
+        $scope.$apply(function () {
+            $scope.topCustomers = orderData.TopCustomers;
+        });
+    });
+
+    angular.element($document[0]).ready(async function () {
+        const productData = await getProductData();
+        $scope.$apply(function () {
+            $scope.topProductsByUnits = productData.TopProductsByUnits;
+            $scope.topProductsByRevenue = productData.TopProductsByRevenue;
+            $scope.displayedProducts = $scope.topProductsByUnits; // Default display
+        });
+    });
+
+    $scope.displayByUnits = function () {
+        $scope.displayedProducts = $scope.topProductsByUnits;
+    };
+
+    $scope.displayByRevenue = function () {
+        $scope.displayedProducts = $scope.topProductsByRevenue;
+    };
+
+}]);
