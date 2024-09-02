63e8307345996dfcf0839b2a4384ed742e0aab40
TIVMOF <tomyivanov06@gmail.com>
2024-09-02 06:00:19 +0000

    chore: Added needed import and extension-point

--- codbex-invoices-widgets/services/unpaid-sales-invoices.js
+++ codbex-invoices-widgets/services/unpaid-sales-invoices.js
@@ -6,6 +6,10 @@ const widgetData = {
     order: 5
 };
 
+export function getWidget() {
+    return widgetData;
+}
+
 if (typeof exports !== 'undefined') {
     exports.getWidgets = function () {
         return widgetData;
